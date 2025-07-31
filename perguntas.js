942715dc7a68924ec07ad9e19385e0af5c401d44
Anderson <anderson.schug@escola.pr.gov.br>
2025-07-30 22:36:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programacão',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França',
+    'Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
